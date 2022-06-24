49729b611f3f80a10672b28911c6de795a51bf3e
James <Jikrebello.za@gmail.com>
2022-06-25 01:46:40 +0200

    Texture Manager Complete

--- DungeonCrawler/Scripts/Manager Classes/TextureManager.cs
+++ DungeonCrawler/Scripts/Manager Classes/TextureManager.cs
@@ -24,21 +24,60 @@ namespace DungeonCrawler
             // First check if the texture has already been created. If so, return that one.
             if (Textures.ContainsKey(key: filePath))
             {
-                Textures[filePath].Item2;
+                return Textures[key: filePath].Item1;
             }
+
+            // At this point the texture doesn't exists, so we'll create and add it.
+            CurrentID++;
+
+            Texture texture;
+            try
+            {
+                texture = new Texture(filename: filePath);
+            }
+            catch (SFML.LoadingFailedException)
+            {
+                return -1;
+            }
+
+            Textures.Add(
+                key: filePath,
+                value: new Tuple<int, Texture>(item1: CurrentID, item2: texture)
+            );
+
+            return CurrentID;
         }
 
         /// <summary>
         /// Removes a texture from the manager from a given id.
         /// </summary>
         /// <param name="textureID">The id of the texture to be removed.</param>
-        public static void RemoveTexture(int textureID) { }
+        public static void RemoveTexture(int textureID)
+        {
+            foreach (var texture in Textures)
+            {
+                if (texture.Value.Item1 == textureID)
+                {
+                    Textures.Remove(key: texture.Key);
+                }
+            }
+        }
 
         /// <summary>
         /// Gets a texture from the texture manager from an ID.
         /// </summary>
         /// <param name="textureID">The id of the texture to return.</param>
-        /// <returns>A reference to the texture.</returns>
-        public static Texture GetTexture(int textureID) { }
+        /// <returns>A reference to the texture. Or null if the texture isn't found.</returns>
+        public static Texture GetTexture(int textureID)
+        {
+            foreach (var texture in Textures)
+            {
+                if (texture.Value.Item1 == textureID)
+                {
+                    return texture.Value.Item2;
+                }
+            }
+            return null;
+        }
     }
 }
