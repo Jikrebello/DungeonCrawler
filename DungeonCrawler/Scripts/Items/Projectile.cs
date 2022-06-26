f45312d75c484ae87fd880414109a0affd9ba9c1
James <Jikrebello.za@gmail.com>
2022-06-26 13:20:00 +0200

    Projectile.cs Done

--- DungeonCrawler/Scripts/Items/Projectile.cs
+++ DungeonCrawler/Scripts/Items/Projectile.cs
@@ -1,1 +1,56 @@
-namespace DungeonCrawler { }
+using SFML.Graphics;
+using SFML.System;
+
+namespace DungeonCrawler
+{
+    public class Projectile : Object
+    {
+        /// <summary>
+        /// The velocity of the projectile.
+        /// </summary>
+        Vector2f _velocity;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="texture">The texture of the projectile.</param>
+        /// <param name="origin">The location that the projectile should be created at.</param>
+        /// <param name="screenCenter">The center of the screen. Used to calculate direction.</param>
+        /// <param name="target">The target location of the projectile.</param>
+        public Projectile(Texture texture, Vector2f origin, Vector2f screenCenter, Vector2f target)
+        {
+            // Create the sprite.
+            SetSprite(texture: texture, isSmooth: false);
+
+            // Set the sprite position.
+            Position = origin;
+
+            // Calculate the velocity of the object.
+            _velocity = target - screenCenter;
+
+            float length = (float)Math.Sqrt(
+                d: Convert.ToDouble(value: _velocity.X * _velocity.X)
+                    + Convert.ToDouble(value: _velocity.Y * _velocity.Y)
+            );
+            _velocity.X /= length;
+            _velocity.Y /= length;
+        }
+
+        /// <summary>
+        /// Update the projectile.
+        /// </summary>
+        /// <param name="_deltaTime">The time in seconds since the last update.</param>
+        public override void Update(float _deltaTime)
+        {
+            // Update rotation.
+            sprite.Rotation += 400f * _deltaTime;
+
+            // Update position.
+            sprite.Position = new Vector2f(
+                x: sprite.Position.X + (_velocity.X * (500 * _deltaTime)),
+                y: sprite.Position.Y + (_velocity.Y * (500 * _deltaTime))
+            );
+            Position = sprite.Position;
+        }
+    }
+}
