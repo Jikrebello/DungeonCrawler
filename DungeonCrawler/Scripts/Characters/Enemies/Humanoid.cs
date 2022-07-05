450b7e119b66a36023c7e10063a3dcd40e9e03f5
James Rebello <james@emperiogroup.co.za>
2022-07-05 23:42:28 +0200

    Humanoid.cs done

--- DungeonCrawler/Scripts/Characters/Enemies/Humanoid.cs
+++ DungeonCrawler/Scripts/Characters/Enemies/Humanoid.cs
@@ -1,1 +1,45 @@
-namespace DungeonCrawler { }
+namespace DungeonCrawler
+{
+    public class Humanoid : Enemy
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Humanoid()
+        {
+            // Load textures.
+            textureIDs[(int)ANIMATION_STATE.Walk_Up] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/skeleton/spr_skeleton_walk_up.png"
+            );
+            textureIDs[(int)ANIMATION_STATE.Walk_Down] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/skeleton/spr_skeleton_walk_down.png"
+            );
+            textureIDs[(int)ANIMATION_STATE.Walk_Right] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/skeleton/spr_skeleton_walk_right.png"
+            );
+            textureIDs[(int)ANIMATION_STATE.Walk_Left] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/skeleton/spr_skeleton_walk_left.png"
+            );
+            textureIDs[(int)ANIMATION_STATE.Idle_Up] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/skeleton/spr_skeleton_idle_up.png"
+            );
+            textureIDs[(int)ANIMATION_STATE.Idle_Down] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/skeleton/spr_skeleton_idle_down.png"
+            );
+            textureIDs[(int)ANIMATION_STATE.Idle_Right] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/skeleton/spr_skeleton_idle_right.png"
+            );
+            textureIDs[(int)ANIMATION_STATE.Idle_Left] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/skeleton/spr_skeleton_idle_left.png"
+            );
+
+            // Set initial sprite.
+            SetSprite(
+                TextureManager.GetTexture(textureID: textureIDs[(int)ANIMATION_STATE.Walk_Up]),
+                isSmooth: false,
+                frames: 8,
+                frameSpeed: 12
+            );
+        }
+    }
+}
