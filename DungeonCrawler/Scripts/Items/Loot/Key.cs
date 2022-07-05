f7096bfc0626b2870e7f4ebb707afc34f5e0cb2b
James Rebello <james@emperiogroup.co.za>
2022-07-05 22:41:59 +0200

    Key.cs done

--- DungeonCrawler/Scripts/Items/Loot/Key.cs
+++ DungeonCrawler/Scripts/Items/Loot/Key.cs
@@ -1,1 +1,28 @@
-namespace DungeonCrawler { }
+namespace DungeonCrawler
+{
+    public class Key : Item
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Key()
+        {
+            // Set the sprite.
+            int textureID = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/loot/key/spr_pickup_key.png"
+            );
+            SetSprite(
+                TextureManager.GetTexture(textureID),
+                isSmooth: false,
+                frames: 8,
+                frameSpeed: 12
+            );
+
+            // Set the item name.
+            Name = "Key";
+
+            // Set the item type.
+            Type = ITEM.Key;
+        }
+    }
+}
