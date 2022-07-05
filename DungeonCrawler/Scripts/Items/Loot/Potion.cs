0e804f6c52da2d59931210efdb68ead9b60b8b65
James Rebello <james@emperiogroup.co.za>
2022-07-05 23:01:17 +0200

    Potion.cs done

--- DungeonCrawler/Scripts/Items/Loot/Potion.cs
+++ DungeonCrawler/Scripts/Items/Loot/Potion.cs
@@ -1,1 +1,51 @@
-namespace DungeonCrawler { }
+namespace DungeonCrawler
+{
+    public class Potion : Item
+    {
+        /// <summary>
+        /// The attack value of the potion.
+        /// </summary>
+        public int AttackValue { get; set; }
+
+        /// <summary>
+        /// The defense value of the potion.
+        /// </summary>
+        public int DefenseValue { get; set; }
+
+        /// <summary>
+        /// The stamina value of the potion.
+        /// </summary>
+        public int StaminaValue { get; set; }
+
+        /// <summary>
+        /// The strength value of the potion.
+        /// </summary>
+        public int StrengthValue { get; set; }
+
+        /// <summary>
+        /// The dexterity value of the potion.
+        /// </summary>
+        public int DexterityValue { get; set; }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Potion()
+        {
+            // Load and set sprite.
+            SetSprite(
+                TextureManager.GetTexture(
+                    TextureManager.AddTexture(
+                        filePath: "../Resources/Textures/loot/potions/spr_potion_stamina.png"
+                    )
+                ),
+                isSmooth: false,
+                frames: 8,
+                frameSpeed: 12
+            );
+
+            // Set the item type.
+            Type = ITEM.Potion;
+        }
+    }
+}
