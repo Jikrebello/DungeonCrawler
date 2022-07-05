52c8cc6d1b69c48af8478e0e2791368109227bf6
James Rebello <james@emperiogroup.co.za>
2022-07-05 23:05:23 +0200

    Heart.cs Done

--- DungeonCrawler/Scripts/Items/Loot/Heart.cs
+++ DungeonCrawler/Scripts/Items/Loot/Heart.cs
@@ -1,1 +1,34 @@
-namespace DungeonCrawler { }
+namespace DungeonCrawler
+{
+    public class Heart : Item
+    {
+        /// <summary>
+        /// The amount of health the heart gives.
+        /// </summary>
+        public int HealthValue { get; set; }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Heart()
+        {
+            // Set item sprite.
+            SetSprite(
+                TextureManager.GetTexture(
+                    TextureManager.AddTexture(
+                        filePath: "../Resources/Textures/loot/heart/spr_pickup_heart.png"
+                    )
+                ),
+                isSmooth: false,
+                frames: 8,
+                frameSpeed: 12
+            );
+
+            // Set health value.
+            HealthValue = 15;
+
+            // Set item type.
+            Type = ITEM.Heart;
+        }
+    }
+}
