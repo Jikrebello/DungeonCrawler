3cb0a80296fe12fde41395dde526fc54e6a82320
James Rebello <james@emperiogroup.co.za>
2022-07-05 23:35:27 +0200

    Enemy.cs and Slime.cs Done

--- DungeonCrawler/Scripts/Characters/Enemies/Enemy.cs
+++ DungeonCrawler/Scripts/Characters/Enemies/Enemy.cs
@@ -1,1 +1,42 @@
-namespace DungeonCrawler { }
+namespace DungeonCrawler
+{
+    public class Enemy : Entity
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Enemy()
+        {
+            var rand = new Random();
+
+            // Set stats.
+            Health = rand.Next(minValue: 40, maxValue: 81);
+            Attack = rand.Next(minValue: 5, maxValue: 11);
+            Defense = rand.Next(minValue: 5, maxValue: 11);
+            Strength = rand.Next(minValue: 5, maxValue: 11);
+            Dexterity = rand.Next(minValue: 5, maxValue: 11);
+            Stamina = rand.Next(minValue: 5, maxValue: 11);
+
+            // Set speed.
+            Speed = rand.Next(minValue: 50, maxValue: 151);
+        }
+
+        /// <summary>
+        /// Applies the given amount of damage to the enemy.
+        /// </summary>
+        /// <param name="damage">The amount of damage to deal to the enemy.</param>
+        public void Damage(int damage)
+        {
+            Health -= damage;
+        }
+
+        /// <summary>
+        /// Checks if the enemy has taken enough damage that they are now dead.
+        /// </summary>
+        /// <returns>True if the enemy is dead.</returns>
+        public bool IsDead()
+        {
+            return Health <= 0;
+        }
+    }
+}
--- DungeonCrawler/Scripts/Characters/Enemies/Slime.cs
+++ DungeonCrawler/Scripts/Characters/Enemies/Slime.cs
@@ -1,1 +1,45 @@
-namespace DungeonCrawler { }
+namespace DungeonCrawler
+{
+    public class Slime : Enemy
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Slime()
+        {
+            // Load Textures.
+            textureIDs[(int)ANIMATION_STATE.Walk_Up] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/slime/spr_slime_walk_up.png"
+            );
+            textureIDs[(int)ANIMATION_STATE.Walk_Down] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/slime/spr_slime_walk_down.png"
+            );
+            textureIDs[(int)ANIMATION_STATE.Walk_Right] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/slime/spr_slime_walk_right.png"
+            );
+            textureIDs[(int)ANIMATION_STATE.Walk_Left] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/slime/spr_slime_walk_left.png"
+            );
+            textureIDs[(int)ANIMATION_STATE.Idle_Up] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/slime/spr_slime_idle_up.png"
+            );
+            textureIDs[(int)ANIMATION_STATE.Idle_Down] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/slime/spr_slime_idle_down.png"
+            );
+            textureIDs[(int)ANIMATION_STATE.Idle_Right] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/slime/spr_slime_idle_right.png"
+            );
+            textureIDs[(int)ANIMATION_STATE.Idle_Left] = TextureManager.AddTexture(
+                filePath: "../Resources/Textures/enemies/slime/spr_slime_idle_left.png"
+            );
+
+            // Set the initial sprite.
+            SetSprite(
+                TextureManager.GetTexture(textureID: textureIDs[(int)ANIMATION_STATE.Walk_Down]),
+                isSmooth: false,
+                frames: 8,
+                frameSpeed: 12
+            );
+        }
+    }
+}
