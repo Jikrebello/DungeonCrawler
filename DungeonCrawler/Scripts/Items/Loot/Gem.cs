14e5326e5c8799a2dc10e85bbbe349d09fde81c0
James Rebello <james@emperiogroup.co.za>
2022-07-05 22:46:55 +0200

    Gem.cs Done

--- DungeonCrawler/Scripts/Items/Loot/Gem.cs
+++ DungeonCrawler/Scripts/Items/Loot/Gem.cs
@@ -1,1 +1,31 @@
-namespace DungeonCrawler { }
+namespace DungeonCrawler
+{
+    public class Gem : Item
+    {
+        public int ScoreValue { get; private set; }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Gem()
+        {
+            // Set the sprite.
+            SetSprite(
+                TextureManager.GetTexture(
+                    TextureManager.AddTexture(
+                        filePath: "../Resources/Textures/loot/gem/spr_pickup_gem.png"
+                    )
+                ),
+                isSmooth: false,
+                frames: 8,
+                frameSpeed: 12
+            );
+
+            // Set the value of the gem.
+            ScoreValue = 50;
+
+            // Set the item type.
+            Type = ITEM.Gem;
+        }
+    }
+}
